d29ed33dfabc6126df2d90aa8a2154111a70b148
liorsultan9 <41189009+liorsultan9@users.noreply.github.com>
2018-07-16 13:15:05 -0700

    Update index.js
    xws

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-var express = require('express');
-var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+
+var http = require('http'); //add the http module
+
+//Create a server
+var myServer = http.createServer(function (request, response) {
+  // Return something from server
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Hello Node\n");
+}); //create a server
+
+Bind server to a port
+myServer.listen(3000);
+
+console.log("Go to localhost:3000 on your browser");
